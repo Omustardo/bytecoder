e8c90e75485a64f0d614f32ec35cd2f57c6f532d
Omar <omustardo@gmail.com>
2017-05-23 02:47:17 -0700

    Support encoding NRGBA colors

--- bytecoder.go
+++ bytecoder.go
@@ -8,6 +8,7 @@ import (
 	"math"
 
 	"github.com/go-gl/mathgl/mgl32"
+	"image/color"
 )
 
 // getByteOrder returns false for LittleEndian and true for BigEndian. It panics if the provided parameter isn't one of the two.
@@ -23,7 +24,7 @@ func getByteOrder(byteOrder binary.ByteOrder) bool {
 	return le
 }
 
-// Bytes returns the byte representation of float32 values in the given byte
+// Float32 returns the byte representation of float32 values in the given byte
 // order. byteOrder must be either binary.BigEndian or binary.LittleEndian.
 func Float32(byteOrder binary.ByteOrder, values ...float32) []byte {
 	le := getByteOrder(byteOrder)
@@ -113,6 +114,58 @@ func Vec3(byteOrder binary.ByteOrder, values ...mgl32.Vec3) []byte {
 		}
 	}
 	return b
+}
+
+// NRGBA returns the byte representation of color.NRGBA values in the given byte
+// order. byteOrder must be either binary.BigEndian or binary.LittleEndian.
+// Note that while NRGBA consists of four uint8 values, this function returns
+// four float32 values, since floating point colors are standard for shaders.
+func NRGBA(byteOrder binary.ByteOrder, values ...color.NRGBA) []byte {
+	le := getByteOrder(byteOrder)
+	width := 16
+	arr := make([]byte, width*len(values))
+	for i, c := range values {
+		r := math.Float32bits(float32(c.R) / 255.0)
+		g := math.Float32bits(float32(c.G) / 255.0)
+		b := math.Float32bits(float32(c.B) / 255.0)
+		a := math.Float32bits(float32(c.A) / 255.0)
+		if le {
+			arr[width*i+0] = byte(r >> 0)
+			arr[width*i+1] = byte(r >> 8)
+			arr[width*i+2] = byte(r >> 16)
+			arr[width*i+3] = byte(r >> 24)
+			arr[width*i+4] = byte(g >> 0)
+			arr[width*i+5] = byte(g >> 8)
+			arr[width*i+6] = byte(g >> 16)
+			arr[width*i+7] = byte(g >> 24)
+			arr[width*i+8] = byte(b >> 0)
+			arr[width*i+9] = byte(b >> 8)
+			arr[width*i+10] = byte(b >> 16)
+			arr[width*i+11] = byte(b >> 24)
+			arr[width*i+12] = byte(a >> 0)
+			arr[width*i+13] = byte(a >> 8)
+			arr[width*i+14] = byte(a >> 16)
+			arr[width*i+15] = byte(a >> 24)
+		} else {
+			arr[width*i+0] = byte(r >> 24)
+			arr[width*i+1] = byte(r >> 16)
+			arr[width*i+2] = byte(r >> 8)
+			arr[width*i+3] = byte(r >> 0)
+			arr[width*i+4] = byte(g >> 24)
+			arr[width*i+5] = byte(g >> 16)
+			arr[width*i+6] = byte(g >> 8)
+			arr[width*i+7] = byte(g >> 0)
+			arr[width*i+8] = byte(b >> 24)
+			arr[width*i+9] = byte(b >> 16)
+			arr[width*i+10] = byte(b >> 8)
+			arr[width*i+11] = byte(b >> 0)
+			arr[width*i+12] = byte(a >> 24)
+			arr[width*i+13] = byte(a >> 16)
+			arr[width*i+14] = byte(a >> 8)
+			arr[width*i+15] = byte(a >> 0)
+		}
+	}
+	return arr
 }
 
 // Uint32 returns the byte representation a uint32 array in the given byte
